c5000f2bc8e509330ba435711b3a17a90a92c287
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2024-12-14 16:06:49 +0100

    fix port

--- composer/ffmpeg.sh
+++ composer/ffmpeg.sh
@@ -3,4 +3,5 @@
 echo "waiting for ${CONTAINER_IP_ADDR}:${WEBRELAY_INTERNAL_TCP_PORT}"
 wait-port "${CONTAINER_IP_ADDR}:${WEBRELAY_INTERNAL_TCP_PORT}"
 # start ffmpeg
+export PULSE_SERVER
 exec ffmpeg -v verbose -f pulse -fragment_size 2000 -ar 44100 -i default -f mpegts -correct_ts_overflow 0 -codec:a mp2 -b:a 128k -ac 1 -muxdelay 0.001 "http://${CONTAINER_IP_ADDR}:${WEBRELAY_INTERNAL_TCP_PORT}/audioout"

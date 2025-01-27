523d80c90a004eff3923350c379d6c110ab668e0
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2025-01-27 10:33:39 +0100

    change  websocket-relay

--- composer/ffmpeg.speaker.sh
+++ composer/ffmpeg.speaker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # create vars
 export CONTAINER_IP_ADDR=${POD_IP:-$(hostname -i)}
 export PULSE_SERVER=${PULSE_SERVER:-/tmp/.pulse.sock}
@@ -15,13 +13,17 @@ fi
 
 echo "$PULSE_SERVER is a unix socket"
 
-# npm install -g wait-port
-# echo "waiting for ${CONTAINER_IP_ADDR}:${WEBRELAY_INTERNAL_TCP_PORT}"
-# wait-port "${CONTAINER_IP_ADDR}:${WEBRELAY_INTERNAL_TCP_PORT}"
-# echo "${CONTAINER_IP_ADDR}:${WEBRELAY_INTERNAL_TCP_PORT} is listening"
-
-# start ffmpeg
-# echo "starting ffmpeg..."
-exec ffmpeg -v verbose -f pulse -fragment_size 2000 -ar 44100 -i auto_null.monitor -f mpegts -correct_ts_overflow 0 -codec:a mp2 -b:a 128k -ac 1 -muxdelay 0.001 "http://${CONTAINER_IP_ADDR}:${WEBRELAY_INTERNAL_TCP_PORT}/audioout"
-~                                                                                                                                                                                                           
-~                               
+#
+# like source https://github.com/phoboslab/jsmpeg
+# juut remove video and define a fifo as output for fffmpeg 
+#
+# description
+# read from pulse 
+# -i speaker.monitor
+# and format to mpeg 2 
+# 1 channel 
+# 128 k
+# live option
+# to a fifo file /container/speaker
+#
+exec ffmpeg -f pulse -fragment_size 2000 -ar 44100 -i speaker.monitor -f mpegts -correct_ts_overflow 0 -codec:a mp2 -b:a 128k -ac 1 -muxdelay 0.001 pipe:1 > /container/speaker

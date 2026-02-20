d5cbcc66bc70145c808bd4b63dab839add0262a0
alexandredevely <12896316+alexandredevely@users.noreply.github.com>
2026-02-20 13:02:18 +0100

    create XDG_RUNTIME_DIR

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,7 +14,10 @@ export DISABLE_RTKIT=${DISABLE_RTKIT:-'y'}
 export DISPLAY=${DISPLAY:'0.0'}
 
 # create XDG_RUNTIME_DIR if not exist
-mkdir -p ${XDG_RUNTIME_DIR}
+if [ ! -d "${XDG_RUNTIME_DIR}" ]; then
+  mkdir -p "${XDG_RUNTIME_DIR}"
+  chmod 0700 "${XDG_RUNTIME_DIR}"
+fi   
 
 # dump for debug
 id  >  ${ABCDESKTOP_LOG_DIR}/docker-entrypoint-pulseaudio.log
